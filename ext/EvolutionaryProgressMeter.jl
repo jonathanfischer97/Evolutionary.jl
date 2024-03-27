a5b7e9aa0d39a759d550efff84f120534a4e970f
Jonathan Fischer PEPE <jfisch27@jhu.edu>
2024-03-26 20:02:09 -0400

    can't get it to work

--- ext/EvolutionaryProgressMeter.jl
+++ ext/EvolutionaryProgressMeter.jl
@@ -1,15 +1,16 @@
 module EvolutionaryProgressMeter
 
     # using Evolutionary, ProgressMeter
-    using ProgressMeter:: @showprogress
+    using ProgressMeter: @showprogress
     using Evolutionary
+    import Evolutionary: value!
 
-    function Evolutionary.value!(obj::Evolutionary.EvolutionaryObjective{TC,TF,TX,Val{:threadprogress}},
-        F::AbstractVector, xs::AbstractVector{TX}) where {TC,TF<:Real,TX}
+    function Evolutionary.value!(obj::Evolutionary.EvolutionaryObjective{TC,TF,TX,Val{:thread}},
+        F::AbstractVector, xs::AbstractVector{TX}) where {TC,TF<:Real,TX<:AbstractVector}
 
         n = length(xs)
         @showprogress Threads.@threads for i in 1:n
-            F[i] = Evolutionary.value(obj, xs[i])
+            F[i] = value(obj, xs[i])
             end
         F
     end
